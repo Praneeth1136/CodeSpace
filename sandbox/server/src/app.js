645353eeed5c32d3c902434c13d0715f35c1654c
Praneeth1136 <praneethkilaparthi@gmail.com>
2026-07-01 04:45:46 +0530

    chore: initialize sandbox deployment configuration and update node_modules dependencies

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
+import { createPod } from "./kubernetes/pod.js";
+import { createService } from "./kubernetes/service.js";
+import { v7 as uuidv7 } from "uuid";
+
 const app = express();
 
 
@@ -17,4 +21,22 @@ app.get("/api/sandbox/health", (req, res) => {
   res.status(200).json({ message: "Sandbox API is healthy" });
 });
 
+app.post("/api/sandbox/start",async(req,res)=>{
+    const sandboxId = uuidv7();
+    try{
+        const pod = await createPod(sandboxId);
+        const service = await createService(sandboxId);
+
+        res.status(200).json({
+            message:"   Container Created successfully",
+            sandboxId,
+            // service,
+            previewUrl:`http://${sandboxId}.preview.localhost`
+        })
+    }
+    catch(error){
+        res.status(500).json({message:"Internal server error"})
+    }
+    
+})
 export default app;
---
+++ sandbox/server/src/kubernetes/pod.js
@@ -0,0 +1,46 @@
+import {k8sCoreV1Api} from "./config.js" 
+import { v4 as uuidv4 } from "uuid";
+
+export async function createPod(sandboxId){
+    const podManifest = {
+        metadata:{
+            name:`sandbox-pod-${sandboxId}`,
+            labels:{
+                app:`sandbox`,
+                id:sandboxId
+            }
+        },
+        spec:{
+            containers:[
+                {
+                    image: "template:latest",
+                    imagePullPolicy: "IfNotPresent",
+                    name: 'sandbox-container',
+                    ports:[
+                        {
+                            containerPort:5173,
+                            name:"web"
+                        }
+                    ],
+                    resources:  {
+                        limits:{
+                            cpu:"500m",
+                            memory:"256Mi"
+                        },
+                        requests:{
+                            cpu:"250m",
+                            memory:"128Mi"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+
+    const response = await k8sCoreV1Api.createNamespacedPod({
+        namespace:"default",
+        body:podManifest
+    });
+
+    return response
+}
